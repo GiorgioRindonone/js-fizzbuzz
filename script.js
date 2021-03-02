a724b5f7d060a388b24138c73a409249185aa6bc
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-02 16:44:45 +0100

    update_finish

--- script.js
+++ script.js
@@ -31,6 +31,30 @@ if (!isNaN(numeridagenerare) ) {
 } else {
   alert("non hai digitato un numero");
   var numeridagenerare = parseInt(prompt("quanti numeri vuoi generare?"));
+  for (var i = 0; i < numeridagenerare; i++) {
+
+    generati.push(i+1) ;
+
+
+      if  (i % 3 == 0 && i % 5 == 0) {
+        console.log("fixbuz");
+        document.getElementById("lista").innerHTML += "<li>" +  "fixbuz" + "</li>";
+      }
+
+      else if (i%3 == 0) {
+        console.log("fix");
+        document.getElementById("lista").innerHTML += "<li>" + "fix" + "</li>";
+      }
+
+      else if (i%5== 0) {
+        console.log("buzz");
+        document.getElementById("lista").innerHTML += "<li>" + "buzz" + "</li>";
+      }
+      else {
+        document.getElementById("lista").innerHTML += "<li>" + generati[i] + "</li>";
+      }
+
+    }
 
 }
     // console.log(generati);
