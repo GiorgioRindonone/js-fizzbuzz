9615be2d9ae91194c85592162fa8c4fefb61307e
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-03-02 16:18:36 +0100

    update_2

--- script.js
+++ script.js
@@ -1,53 +1,36 @@
-var listanum = document.getElementById("lista");
+// var listanum = document.getElementById("lista");
 
 var numeridagenerare = parseInt(prompt("quanti numeri vuoi generare?"));
 var generati = [];
 
-if (isNaN(numeridagenerare) == false) {
+if (!isNaN(numeridagenerare) ) {
   for (var i = 0; i < numeridagenerare; i++) {
 
     generati.push(i+1) ;
 
-    // listanum.innerHTML = "";
 
-    if  (i % 3 == 0 && i % 5 == 0) {
-      console.log("fixbuz");
-    }
+      if  (i % 3 == 0 && i % 5 == 0) {
+        console.log("fixbuz");
+        document.getElementById("lista").innerHTML += "<li>" +  "fixbuz" + "</li>";
+      }
 
-    else if (i%3 == 0) {
-      console.log("fix");
-    }
+      else if (i%3 == 0) {
+        console.log("fix");
+        document.getElementById("lista").innerHTML += "<li>" + "fix" + "</li>";
+      }
 
-    else if (i%5== 0) {
-      console.log("buzz");
-    }
-
-    document.getElementById("lista").innerHTML += "<li>" + generati[i] + "</li>";
+      else if (i%5== 0) {
+        console.log("buzz");
+        document.getElementById("lista").innerHTML += "<li>" + "buzz" + "</li>";
+      }
+      else {
+        document.getElementById("lista").innerHTML += "<li>" + generati[i] + "</li>";
+      }
 
     }
 } else {
   alert("non hai digitato un numero");
   var numeridagenerare = parseInt(prompt("quanti numeri vuoi generare?"));
-  for (var i = 0; i < numeridagenerare; i++) {
 
-    generati.push(i+1) ;
-
-    // listanum.innerHTML = "";
-
-    if  (i % 3 == 0 && i % 5 == 0) {
-      console.log("fixbuz");
-    }
-
-    else if (i%3 == 0) {
-      console.log("fix");
-    }
-
-    else if (i%5== 0) {
-      console.log("buzz");
-    }
-
-    listanum.innerHTML += "<li>" + generati[i] + "</li>";
-
-    }
 }
-    console.log(generati);
+    // console.log(generati);
